474dd48847e4e4a64bbfac8ba940819a3045d578
MinLinking <108618469+Minglink@users.noreply.github.com>
2026-07-30 03:13:54 +0800

    Create __init__.py

---
+++ src/factor_registry/evaluation/__init__.py
@@ -0,0 +1,5 @@
+"""Evaluation modules appropriate to each factor family."""
+
+from factor_registry.evaluation.cross_sectional import EvaluationReport, evaluate_cross_sectional
+
+__all__ = ["EvaluationReport", "evaluate_cross_sectional"]
